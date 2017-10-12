c492785737debe6b09596abdb67f901811682af2
Manuel Riezebosch <mriezebosch@gmail.com>
2017-10-12 16:38:14 +0200

    using stryker typescript transpiler

--- stryker.conf.js
+++ stryker.conf.js
@@ -1,17 +1,23 @@
-module.exports = function(config){
+module.exports = function(config) {
   config.set({
     files: [
-        // Add your files here, this is just an example:
-        { pattern: 'src/**/*.js', mutated: true, included: false},
-        '!src/index.js',
-        'test/**/*.js',
-        { pattern: 'test/test_data/**/*', mutated: false, included: false },
-        { pattern: 'src/template/*', mutated: false, included: false },
-        { pattern: 'src/theme/*', mutated: false, included: false },
+      '!bin/**',
+      '!src/**/*.ts',
+      {
+        pattern: "src/**/*.ts",
+        mutated: true,
+        included: false
+      },
+      "test/**/*.ts",
+      { pattern: "test/test_data/**", mutated: false, included: false }
     ],
-    testRunner: 'mocha',
-    testFramework: 'mocha',
-    coverageAnalysis: 'all',
-    reporter: ['clear-text', 'html', 'progress', 'baseline']
+    testRunner: "mocha",
+    reporter: ["html", "baseline", "clear-text", "progress"],
+    testFramework: "mocha",
+    coverageAnalysis: "off",
+    transpilers: [ 'typescript' ],
+    tsconfigFile: 'tsconfig.json',
+    mutator: 'typescript',
+    loglevel: 'debug'
   });
-}
+};
